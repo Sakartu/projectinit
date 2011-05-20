023cf40939ca3d71d3368d3fb1ff689e335b900e
Peter Wagenaar <sakartu@gmail.com>
2011-05-20 19:01:59 +0200

    initial setup

---
+++ projectinit.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+TEMPLATE_HOME = '~/.templates'
+
+import sys
+
+def main():
+	if len(sys.argv) < 2:
+		usage()
+	
+	proj_template = sys.argv[1]
+
+	for template in os.path:
+		pass
+		
+
+
+def usage():
+	sys.exit(0)
+
+if __name__ == '__main__':
+	main()
